81f28fdc290c91c34131fad44d527a06337ba835
kyw <rladyddn258@gmail.com>
2025-02-07 16:01:43 +0900

    윈도우 설정창 참고해서 디자인하는중

--- DuratioinAnimationExample/MainWindow.xaml.cs
+++ DuratioinAnimationExample/MainWindow.xaml.cs
@@ -1,10 +1,16 @@
-﻿using System.Windows;
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
 
-namespace SlideMenuBarExample
+namespace DuratioinAnimationExample
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
@@ -14,8 +20,8 @@ namespace SlideMenuBarExample
         public MainWindow()
         {
             InitializeComponent();
-        }
 
-       
+            
+        }
     }
 }
---
+++ SlideMenuBarExample/ViewModels/MainViewModel.cs
@@ -0,0 +1,39 @@
+﻿using SlideMenuBarExample.Helper;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace SlideMenuBarExample.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        private ImageSource invertedhomeimage;
+     
+
+        public ImageSource InvertedHomeImage
+        {
+            get
+            {
+                return invertedhomeimage;
+            }
+            set
+            {
+                if(invertedhomeimage != value)
+                {
+                    invertedhomeimage = value;
+                    OnPropertyChanged(nameof(InvertedHomeImage));
+                }
+            }
+        }
+
+        public MainViewModel()
+        {
+            // 원본 이미지 불러오기 (Build Action이 Content이고, Copy to Output Directory가 설정되어 있어야 한다)
+            BitmapImage originalImage = new BitmapImage(new Uri("ico/home.ico", UriKind.Relative));
+            InvertedHomeImage = ImageHelper.InvertImage(originalImage);
+            //originalImage = new BitmapImage(new Uri("ico/home.ico", UriKind.Relative));
+        }
+
+
+
+    }
+}
